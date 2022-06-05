a560070d403be47a9313fa8690d0b77cd5f3aac4
Nicole <ngott30@gmail.com>
2022-06-05 10:10:32 +0200

    added references in main and added button listener

--- app/src/main/java/com/example/studio_sqlite/MainActivity.java
+++ app/src/main/java/com/example/studio_sqlite/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.studio_sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //References to buttons and other controls on the layout
+    Button btn_add;
+    EditText et_todo, et_descrip;
+    Switch sw;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assign values to variables
+        btn_add = findViewById(R.id.btn_add);
+        et_todo = findViewById(R.id.et_todo);
+        et_descrip = findViewById(R.id.et_descrip);
+        sw = findViewById(R.id.sw);
+        lv = findViewById(R.id.lv);
+
+        //button click listeners
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Added", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
